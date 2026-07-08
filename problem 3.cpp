34566fbe6bf23365a2a1444d5a438e08a738c234
zihad-max <zihadkun2007@gmail.com>
2026-07-08 21:13:47 +0600

    Add files via upload

---
+++ problem 3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int ascii;
+char character;
+printf("Enter a ascii value: ");
+scanf("%d",&ascii);
+character=ascii;
+printf(" Equivalent character is=%c",character);
+return 0;
+}
