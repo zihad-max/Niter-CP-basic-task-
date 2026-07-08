ec3ff5f2f0f866ec590f09e0c83c24b95f304632
zihad-max <zihadkun2007@gmail.com>
2026-07-08 21:27:24 +0600

    Add files via upload

---
+++ problem 8 .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+float area;
+printf("Enter rectangle side a and b length :");
+scanf("%d %d",&a ,&b);
+area=a*b;
+printf("area is= %.2f",area);
+return 0;
+}
